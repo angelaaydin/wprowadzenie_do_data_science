11c86600396e8adc4eb52da033f178599ae4b49f
angelaaydin <andzelika.aydin@gmail.com>
2025-03-27 15:45:42 +0000

    file rename

---
+++ python_m1.py
@@ -0,0 +1,14 @@
+print ('hello, world')
+
+if x > 5:
+    print('Hello')
+    x=6
+
+    # breaking line with paranthesis () or backshash /
+
+    long_string = (
+            'This is a very long string'
+            'that spans multiple lanes.'
+)
+    
+    #
